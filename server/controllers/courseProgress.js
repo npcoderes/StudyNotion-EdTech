91689e07fa59aa94bcce1d55ba7122087e2f3de4
npcoderes <nikulpanchal2505@gmail.com>
2025-02-07 18:36:18 +0530

    com

--- server/controllers/courseProgress.js
+++ server/controllers/courseProgress.js
@@ -85,7 +85,7 @@ exports.updateCourseProgress = async (req, res) => {
     return res.status(200).json({ message: "Course progress updated" });
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ error: "Internal server error" });
+    return res.status(500).json({ error: "Internal server error........."+error });
   }
 };
 exports.getProgressPercentage = async (req, res) => {
